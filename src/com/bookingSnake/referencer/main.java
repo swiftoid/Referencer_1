ba30f281c15fa9b0dff75fb4b39bf484faf6b070
Andrew Swift (SWI94000733) <SWI94000733@Hull-College.ac.uk>
2015-12-01 13:45:35 +0000

    Initial commit

---
+++ src/com/bookingSnake/referencer/main.java
@@ -0,0 +1,22 @@
+package com.bookingSnake.referencer;
+
+/**
+ * Created by swi94000733 on 01/12/2015.
+ */
+public class main {
+
+    public static void main (String args[]){
+
+
+        String foo = "Hello";
+
+        System.out.println(foo);
+
+
+
+        
+
+
+
+    }
+}
